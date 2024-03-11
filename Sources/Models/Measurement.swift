213bc15ea2828054eabc80b57f01de1aa02c9fe9
Zhennan Zhou <zhouz46@uw.edu>
2024-03-10 17:57:53 -0700

    add license info

--- Sources/Models/Measurement.swift
+++ Sources/Models/Measurement.swift
@@ -1,8 +1,13 @@
 //
-//  File.swift
-//  
+// This source file is part of the LCLPing open source project
 //
-//  Created by Zhennan Zhou on 2/28/24.
+// Copyright (c) 2021-2024 Local Connectivity Lab and the project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of project authors
+//
+// SPDX-License-Identifier: Apache-2.0
 //
 
 import Foundation
