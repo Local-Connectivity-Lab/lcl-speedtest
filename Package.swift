efa93f926f64f95141c478f5980fc8331281960a
Zhennan Zhou <zhouz46@uw.edu>
2024-09-25 23:29:11 -0700

    bump swiftnio version

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
         .library(name: "LCLSpeedtest", targets: ["LCLSpeedtest"])
     ],
     dependencies: [
-        .package(url: "https://github.com/johnnzhou/swift-nio.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.73.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-        .package(url: "https://github.com/johnnzhou/websocket-kit.git", branch: "main")
+        .package(url: "https://github.com/Local-Connectivity-Lab/websocket-kit.git", branch: "main")
     ],
     targets: [
         .target(
