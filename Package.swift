6d8bd159b2260b83de4f95a7b718724d0d156a64
Zhennan Zhou <zhouz46@uw.edu>
2024-10-01 10:49:44 -0700

    bump to stable version of websocket

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.73.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-        .package(url: "https://github.com/Local-Connectivity-Lab/websocket-kit.git", branch: "main")
+        .package(url: "https://github.com/Local-Connectivity-Lab/websocket-kit.git", from: "1.0.0")
     ],
     targets: [
         .target(
