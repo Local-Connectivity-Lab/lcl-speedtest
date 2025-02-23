1b5a79555025c0646cf88a8290c42d9c26e616b4
John Zhou <37914490+johnnzhou@users.noreply.github.com>
2025-02-23 00:12:47 -0800

    Update lcl-websocket to 1.1.1 (#11)

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-        .package(url: "https://github.com/Local-Connectivity-Lab/lcl-websocket.git", from: "1.1.0"),
+        .package(url: "https://github.com/Local-Connectivity-Lab/lcl-websocket.git", from: "1.1.1"),
     ],
     targets: [
         .target(
